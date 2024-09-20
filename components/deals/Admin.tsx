6d375f74e3651ad43755568cc03be0904d349223
Memn0n <shalomphiriviii@gmal.com>
2024-09-20 22:43:58 +0200

    some update

--- components/deals/Admin.tsx
+++ components/deals/Admin.tsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Admin = () => {
   return (
     <div>
-    <div className='bg-white border border-black rounded-3xl w-[300px] h-[480px] relative'>
-   <h1 className='pt-2 text-center text-2xl drop-shadow-l font-semibold pb-4'>Admin Websites</h1>
-   <ol type='i' className='mx-10'>
-     <li className='text-xl pb-1 font-normal'>- Non-Responsive</li>
-     <li className='text-xl pb-1 font-normal'>- Multi-Page</li>
-     <li className='text-xl pb-1 font-normal'>- Admin Sign-up/Sign-in</li>
-     <li className='text-xl pb-1 font-normal'>- Content Management</li>
-     <li className='text-xl pb-1 font-normal'>- FullStack</li>
-     <li className='text-xl pb-1 font-normal'>- Free Logo</li>
-     <li className='text-xl pb-1 font-normal'>- 2 Free Update</li>
-   </ol>
+      <div className="bg-white border border-black rounded-3xl w-[300px] h-[480px] relative">
+        <h1 className="pt-2 text-center text-2xl drop-shadow-l font-semibold pb-4">
+          Admin Websites
+        </h1>
+        <ol type="i" className="mx-10">
+          <li className="text-xl pb-1 font-normal">- Non-Responsive</li>
+          <li className="text-xl pb-1 font-normal">- Multi-Page</li>
+          <li className="text-xl pb-1 font-normal">- Admin Sign-up/Sign-in</li>
+          <li className="text-xl pb-1 font-normal">- Content Management</li>
+          <li className="text-xl pb-1 font-normal">- FullStack</li>
+          <li className="text-xl pb-1 font-normal">- Free Logo</li>
+          <li className="text-xl pb-1 font-normal">- 2 Free Update</li>
+        </ol>
 
-   <h2 className='absolute bottom-2 text-center w-full font-semibold text-2xl'>
-     R449.99
-     <p className='text-sm font-light'>once off</p>
-   </h2>
- </div>
- </div>
-  )
-}
+        <h2 className="absolute bottom-2 text-center w-full font-semibold text-2xl">
+          R1249.99
+          <p className="text-sm font-light">once off</p>
+        </h2>
+      </div>
+    </div>
+  );
+};
 
-export default Admin
+export default Admin;
